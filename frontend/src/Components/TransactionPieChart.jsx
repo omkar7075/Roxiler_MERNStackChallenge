f8076bd99fe73bc92fb41dde57866a74810bfd28
Omkar (Ahaan) Waghmare <91741647+omkar7075@users.noreply.github.com>
2024-11-19 15:31:35 +0530

    TransactionPieChart.jsx

---
+++ frontend/src/Components/TransactionPieChart.jsx
@@ -0,0 +1,51 @@
+import '../App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF'];
+
+const TransactionPieChart = ({ selectedMonth }) => {
+  const [pieChartData, setPieChartData] = useState([]);
+
+  useEffect(() => {
+    const fetchPieChartData = async () => {
+      try {
+        const response = await axios.get(
+          'https://roxiler-mernstackchallenge.onrender.com/api/pie-chart',
+          { params: { month: selectedMonth } }
+        );
+        setPieChartData(response.data);
+      } catch (error) {
+        console.error('Error fetching pie chart data:', error);
+      }
+    };
+    fetchPieChartData();
+  }, [selectedMonth]);
+
+  return (
+    <div>
+      <h1>Pie Chart:</h1>
+      <PieChart width={600} height={300}>
+        <Pie
+          data={pieChartData}
+          dataKey="value"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          outerRadius={100}
+          fill="#8884d8"
+          label
+        >
+          {pieChartData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend />
+      </PieChart>
+    </div>
+  );
+};
+
+export default TransactionPieChart;
