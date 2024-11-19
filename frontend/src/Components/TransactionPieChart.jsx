5c769a9d249119886d507afc367bb2fe789264e6
Omkar (Ahaan) Waghmare <91741647+omkar7075@users.noreply.github.com>
2024-11-19 16:03:47 +0530

    TransactionPieChart.jsx

--- frontend/src/Components/TransactionPieChart.jsx
+++ frontend/src/Components/TransactionPieChart.jsx
@@ -1,9 +1,8 @@
-import '../App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
+import { PieChart, Pie, Tooltip, Legend, Cell } from 'recharts';
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF'];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF', '#FF4567', '#6A5ACD'];
 
 const TransactionPieChart = ({ selectedMonth }) => {
   const [pieChartData, setPieChartData] = useState([]);
@@ -11,40 +10,40 @@ const TransactionPieChart = ({ selectedMonth }) => {
   useEffect(() => {
     const fetchPieChartData = async () => {
       try {
-        const response = await axios.get(
-          'https://roxiler-mernstackchallenge.onrender.com/api/pie-chart',
-          { params: { month: selectedMonth } }
-        );
+        const response = await axios.get('https://roxiler-mernstackchallenge.onrender.com/api/pie-chart', {
+          params: { month: selectedMonth },
+        });
         setPieChartData(response.data);
       } catch (error) {
         console.error('Error fetching pie chart data:', error);
       }
     };
+
     fetchPieChartData();
   }, [selectedMonth]);
 
   return (
-   <div>
-  <h1>Pie Chart:</h1>
-  <PieChart width={600} height={300}>
-    <Pie
-      data={pieChartData}
-      dataKey="count" // Corrected to reflect the numeric data key
-      nameKey="range" // Added to display the labels correctly
-      cx="50%"
-      cy="50%"
-      outerRadius={100}
-      fill="#8884d8"
-      label
-    >
-      {pieChartData.map((entry, index) => (
-        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-      ))}
-    </Pie>
-    <Tooltip />
-    <Legend />
-  </PieChart>
-</div>
+    <div>
+      <h1>Pie Chart:</h1>
+      <PieChart width={600} height={300}>
+        <Pie
+          data={pieChartData}
+          dataKey="count"
+          nameKey="category"
+          cx="50%"
+          cy="50%"
+          outerRadius={100}
+          fill="#8884d8"
+          label
+        >
+          {pieChartData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend />
+      </PieChart>
+    </div>
   );
 };
 
